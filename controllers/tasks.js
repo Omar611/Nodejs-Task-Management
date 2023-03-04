e0832d5685f50156e43e52e53deb78b506e77b85
Omar Tarek <omartarek711@gmail.com>
2023-03-04 22:19:25 +0200

    Add empty routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,27 @@
 const getALlTasks = (req, res) => {
-	res.send("Hello from tasks API Controller");
+	res.send("Get all tasks");
 };
 
-module.exports = { getALlTasks };
+const getTask = (req, res) => {
+	res.json(req.params.id);
+};
+
+const createTask = (req, res) => {
+	res.json(req.body);
+};
+
+const updateTask = (req, res) => {
+	res.send("Update task");
+};
+
+const deleteTask = (req, res) => {
+	res.send("Delete task");
+};
+
+module.exports = { 
+	getALlTasks,
+	getTask,
+	createTask,
+	updateTask,
+	deleteTask
+ };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getALlTasks} = require('../controllers/tasks');
+const {getALlTasks, getTask, createTask, updateTask, deleteTask} = require('../controllers/tasks');
 
 router.route('/').get(getALlTasks);
+router.route('/:id').get(getTask);
+router.route('/').post(createTask);
+router.route('/:id').put(updateTask);
+router.route('/:id').delete(deleteTask);
 
 module.exports = router;
