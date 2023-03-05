58df07d34cda0daab97981797fc68e3458114e8c
Omar Tarek <omartarek711@gmail.com>
2023-03-06 00:01:26 +0200

    Complete crud operation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,36 +1,62 @@
 const Task = require("../models/Task");
 
-const getALlTasks = (req, res) => {
-	res.send("Get all tasks");
+const getALlTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json(tasks);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error });
+	}
 };
 
-const getTask = (req, res) => {
-	res.json(req.params.id);
+const getTask = async (req, res) => {
+	try {
+		const task = await Task.find({ _id: req.params.id });
+		if (!task) return res.status(404).json({ error: "Task not found" });
+		res.status(200).json(task);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error });
+	}
 };
 
 const createTask = async (req, res) => {
 	try {
 		const task = await Task.create(req.body);
 		res.status(201).json({ task });
-	}
-	catch (error) {
+	} catch (error) {
 		console.log(error);
 		res.status(500).json({ error });
 	}
 };
 
-const updateTask = (req, res) => {
-	res.send("Update task");
+const updateTask = async (req, res) => {
+	try {
+		const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+		if (!task) return res.status(404).json({ error: "Task not found" });
+		res.status(200).json({ message: "Task updated" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error });
+	}
 };
 
-const deleteTask = (req, res) => {
-	res.send("Delete task");
+const deleteTask = async (req, res) => {
+	try {
+		const task = await Task.findByIdAndDelete(req.params.id);
+		if (!task) return res.status(404).json({ error: "Task not found" });
+		res.status(200).json({ message: "Task deleted" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error });
+	}
 };
 
-module.exports = { 
+module.exports = {
 	getALlTasks,
 	getTask,
 	createTask,
 	updateTask,
-	deleteTask
- };
+	deleteTask,
+};
