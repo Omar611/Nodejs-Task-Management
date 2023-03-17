77dfc7cf3e2331bac5e5cf2a062fa5da513a365d
Omar Tarek <omartarek711@gmail.com>
2023-03-17 20:45:05 +0200

    enhance router structure

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {getALlTasks, getTask, createTask, updateTask, deleteTask} = require('../controllers/tasks');
+const { getALlTasks, getTask, createTask, updateTask, deleteTask } = require("../controllers/tasks");
 
-router.route('/').get(getALlTasks);
-router.route('/:id').get(getTask);
-router.route('/').post(createTask);
-router.route('/:id').put(updateTask);
-router.route('/:id').delete(deleteTask);
+router.route("/").get(getALlTasks).post(createTask);
+router.route("/:id").get(getTask).put(updateTask).delete(deleteTask);
 
 module.exports = router;
