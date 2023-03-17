002ff45726e59f3c5e93bbe0e76ea314e45d1f52
Omar Tarek <omartarek711@gmail.com>
2023-03-17 21:05:09 +0200

    Enhance 404 function

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
 
+app.use(express.static('./public'));
 app.use(express.json());
+
+app.use("/api/v1/tasks", tasks);
 
 app.get("/", (req, res) => {
 	res.send("Task Manager API");
 });
 
-app.use("/api/v1/tasks", tasks);
+app.use(notFound);
 
-app.all("*", (req, res) => {
-	res.status(404).send("Page not found");
-});
 
 const port = 3000;
 
